f2296bfe34dd223317997a3edc027f490f0fa9d1
yj <254651426@qq.com>
2020-05-14 09:50:40 +0800

    增加了一个网友答案

--- Leetcode/src/main/java/org/yangjie/com/Leetcode/Suzuquchong.java
+++ Leetcode/src/main/java/org/yangjie/com/Leetcode/Suzuquchong.java
@@ -14,7 +14,7 @@ public class Suzuquchong {
 	public static void main(String[] args) {
 		int[] num = { 4, 3, 2, 7, 8, 2, 3, 1 };
 
-		findDuplicates(num);
+		System.out.println(findDuplicates1(num));
 	}
 
 	// 暴利破解
@@ -31,4 +31,21 @@ public class Suzuquchong {
 		}
 		return l;
 	}
+
+	/**
+	 *  网友答案
+	 * @param nums
+	 * @return
+	 */
+	public static List<Integer> findDuplicates1(int[] nums) {
+		List<Integer> res = new ArrayList<Integer>();
+		for (int i = 0; i < nums.length; ++i) {
+			int index = Math.abs(nums[i]) - 1;    //用数组元素减1 来做索引  反查数组元素
+			if (nums[index] < 0) 					//如果为负数则说明重复。
+				res.add(Math.abs(index + 1));   
+			nums[index] = -nums[index];				//不为负数 就改成负数
+		}
+		return res;
+	}
+
 }
